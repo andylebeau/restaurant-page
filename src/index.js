f8e1932d0bc2ee2319865ea55df1cfdd1de710d1
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-08-18 16:59:51 -0400

    Added a class tothe id content to represent what page is loaded.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import createHome from "./home-page";
 import createMenu from "./menu-page";
 import createContact from "./contact-page";
 
-
 const getHeader = createHeader();
 document.body.prepend(getHeader);
 
 const content = document.getElementById("content");
+content.classList.add("home")
 content.appendChild(createHome());
 
 changePage();
@@ -16,14 +16,19 @@ function changePage() {
     const pageSelection = document.querySelectorAll(".button-nav")
         pageSelection.forEach(btn => {
             btn.addEventListener('click', loadNewPage)
-        })
+        });
 }
 
 function loadNewPage(e) {
     content.textContent = "";
-    let page = e.target.textContent;
-    if (page === "Menu") { page = createMenu() }
-    else if (page === "Contact") { page = createContact() }
-    else page = page = createHome();
-    content.appendChild(page)
+    content.removeAttribute('class');
+
+    let page = e.target.textContent.toLowerCase();
+    content.classList.add(`${page}`)
+
+    page === "menu" ? page = createMenu() :
+    page === "contact" ? page = createContact() :
+    page = createHome();
+
+    content.appendChild(page);
 }
--- src/menu-page.js
+++ src/menu-page.js
@@ -1,13 +1,27 @@
 function createMenu() {
     const pageContent = document.createElement("div");
     pageContent.classList.add('page-content');
-    
-    const data = document.createElement("p");
-    data.textContent = "MENU PAGE";
 
-    pageContent.appendChild(data);
-    
+    const items = ["Nigiri", "Maki", "Sashimi"];
+    const descriptions = [
+        "Nigiri sushi is that familiar style of sushi made up of an oval-shaped mound of rice with a slice of (usually) raw fish on top. The word nigiri comes from the Japanese nigirizushi, which translates as 'hand-pressed sushi.'",
+        "Maki is shaped like a tube, held together by a sheet of dried seaweed called nori, with a layer of rice and then a layer of fillings, which can include ingredients such as raw fish, cooked shellfish, vegetables, and cream cheese.",
+        "Sashimi is simply strips of raw fish with no rice at all (although rice can accompany sashimi). As such, sashimi is not technically a type of sushi since sushi always features rice. But you can think of nigiri sushi as a hand-pressed mound of rice with a strip of sashimi on top. Unlike nigiri and maki, sashimi is eaten with chopsticks."
+    ]
+    let data = ""
+    items.forEach((item, index) => {
+        data += `<h2 class="menu-item">${item}</h2>
+                 <p class="description">${descriptions[index]}</p>`
+        console.log(item)
+      })
+      pageContent.innerHTML = data
+
     return pageContent;
 }
+
+// function menuItem(item) {
+//     const itemHTML = `<p class="menu-item">${item}/p>`
+//     return itemHTML
+// }
 
 export default createMenu;
