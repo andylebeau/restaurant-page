f5911819fb3e3c37e2963c5a68466492e96fb891
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-08-20 16:07:25 -0400

    Page content complete

--- src/contact-page.js
+++ src/contact-page.js
@@ -2,12 +2,48 @@ function createContact() {
     const pageContent = document.createElement("div");
     pageContent.classList.add('page-content');
     
-    const data = document.createElement("p");
-    data.textContent = "CONTACT PAGE";
+    const contactInfo = document.createElement("div");
+    contactInfo.classList.add('info', 'box');
+    contactInfo.innerHTML = getContactInfo();
 
-    pageContent.appendChild(data);
+    const contactForm = document.createElement("div");
+    contactForm.classList.add('form', 'box');
+    contactForm.innerHTML = getContactForm();
+
+    pageContent.appendChild(contactInfo);
+    pageContent.appendChild(contactForm);
     
     return pageContent;
+}
+
+function getContactInfo() {
+    return (
+        `<h2>Contact Info</h2>
+        <br />
+        <h3>Paddy Fields</h3>
+        <address>123 Tokyo Blvd.<br />
+        New York, NY 12345</address>
+        <p class="phone">555-123-1234</p>
+        <br />
+        <p class="email">contact@paddyfields.com</p>`
+    )
+}
+
+function getContactForm() {
+    return (
+        `<h2>Subscription</h2>
+        <br />
+        <form action="" method="get" class="form">
+          <label for="name">Enter your name: </label>
+          <input type="text" name="name" id="name" required />
+
+          <label for="email">Enter your email: </label>
+          <input type="email" name="email" id="email" required />
+
+          <input type="submit" value="Subscribe!" />
+
+      </form>`
+    )
 }
 
 export default createContact;
--- src/home-page.js
+++ src/home-page.js
@@ -6,12 +6,17 @@ function createHome() {
     image.classList.add('chef');
     image.src = "images/wyron-a-Lhb1DyyNr7U-unsplash.jpg"
     image.alt = "Sushi Chef"
-    
-    const data = document.createElement("q");
-    data.textContent = "This hidden gem has left the best impression! Hospitable hosts, delicious authentic dishes, beautiful presentation, full bar and extravagant desserts. I recommend it to everyone! Simply put, everything was delicious!";
 
+    const slogan = document.createElement("h2");
+    slogan.classList.add('slogan');
+    slogan.textContent = "Classic. Real. Japanese sushi."
+    
+    const review = document.createElement("q");
+    review.textContent = "This hidden gem has left the best impression! Hospitable hosts, delicious authentic dishes, beautiful presentation, full bar and extravagant desserts. I recommend it to everyone! Simply put, everything was delicious!";
+
+    pageContent.appendChild(slogan);
     pageContent.appendChild(image);
-    pageContent.appendChild(data);
+    pageContent.appendChild(review);
     
     return pageContent;
 }
